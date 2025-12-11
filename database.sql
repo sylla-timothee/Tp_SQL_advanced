e7603d3257113effc2d66d8607fd841c7747f1d9
Y0LRIN <chloe.arcadia.alx@gmail.com>
2025-12-11 16:34:22 +0100

    Populating db (dummies)

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+/* Create initial tables */
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS Restaurants (
@@ -40,7 +41,44 @@ CREATE TABLE IF NOT EXISTS OrderItems (
     FOREIGN KEY (IdDishes) REFERENCES Dishes(IdDishes)
 );
 
+/* Required modifications */
 ALTER TABLE Employees ADD COLUMN Hire_date TEXT NOT NULL;
 ALTER TABLE Dishes ADD COLUMN Is_vegan BOOLEAN;
 ALTER TABLE Orders RENAME TO CustomerOrders;
 
+/* Insert data into the tables */
+INSERT INTO Restaurants (IdRestaurant, Name, Planet, Opening_year) 
+    VALUES
+        (1, 'The Pizza Place', 'Earth', 2020),
+        (2, 'The Burger Place', 'Mars', 2021),
+        (3, 'The Ice Cream Place', 'Venus', 2022),
+        (4, 'The Hot Dog Place', 'Mercury', 2023),
+        (5, 'The Taco Place', 'Jupiter', 2024),
+        (6, 'The Sushi Place', 'Saturn', 2025),
+        (7, 'The Ramen Place', 'Uranus', 2026),
+        (8, 'The Pizza Place', 'Neptune', 2027),
+        (9, 'The Burger Place', 'Pluto', 2028),
+        (10, 'The Ice Cream Place', 'Neptune', 2029);
+
+INSERT INTO Dishes (IdDishes, Name, Price, Category, Is_vegan)
+    VALUES
+        (1, 'Margerita', 6.50, 'Pizza', True),
+        (2, 'Capricciosa', 7.50, 'Pizza', True),
+        (3, 'Regina', 9, 'Pizza', False),
+        (4, 'New Yorker', 8, 'Burger', False),
+        (5, 'Chicken', 5, 'Burger', False),
+        (6, 'Miso Ramen', 10, 'Ramen', True),
+        (7, 'Shoyu Ramen', 10, 'Ramen', False);
+
+INSERT INTO Employees (IdEmployees, Firstname, Lastname, Role, IdRestaurant, Hire_date)
+    VALUES
+        (1, 'John', 'Doe', 'Manager', 1, '2020-01-01'),
+        (2, 'Jane', 'Doe', 'Manager', 2, '2021-01-01'),
+        (3, 'Bob', 'Smith', 'Manager', 3, '2022-01-01'),
+        (4, 'Alice', 'Smith', 'Manager', 4, '2023-01-01'),
+        (5, 'Bob', 'Smith', 'Manager', 5, '2024-01-01'),
+        (6, 'Alice', 'Smith', 'Manager', 6, '2025-01-01'),
+        (7, 'Bob', 'Smith', 'Manager', 7, '2026-01-01'),
+        (8, 'Alice', 'Smith', 'Manager', 8, '2027-01-01'),
+        (9, 'Bob', 'Smith', 'Manager', 9, '2028-01-01'),
+        (10, 'Alice', 'Smith', 'Manager', 10, '2029-01-01');
